5bf354c5184f5cd92e3427d71d71b27f1d500d3f
kaajoj <kaajooj@gmail.com>
2020-02-21 14:18:04 +0100

    task3 filter

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -9,22 +10,30 @@ import static junit.framework.TestCase.assertEquals;
 public class Task3 {
 
     private WebDriver driver;
+//    private String filter;
 
     @Before
     public void settings(){
         driver = TestSuite.driver;
+
+
+//        filter = "H1 FY20";
     }
 
     @Test
     public void task3() {
         // 1)
-
+        driver.findElement(By.xpath("//span[@id='uniqName_51_11']/span[3]")).click();
+        driver.findElement(By.id("dijit__TreeNode_4_label")).click();
+//        WebElement changeDate = driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div/div/div[2]/div/div/div[1]/div[2]/div/div[4]/div/div[1]/span[2]/span/span/span[2]"));
+//        changeDate.click();
+//        changeDate.sendKeys(filter);
 
 //        WebDriverWait wait = new WebDriverWait(driver, 30);
 //        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div/ul/li/div/a")));
 
 
-//        driver.findElement(By.id("testWait"));
+        driver.findElement(By.id("testWait"));
     }
 
 //    @After
--- src/main/java/TestSuite.java
+++ src/main/java/TestSuite.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 @Suite.SuiteClasses({
         Task1.class,
         Task2.class,
-
+        Task3.class,
 })
 
 
